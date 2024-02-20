d79744a664581bb621bc3454d9c2e9120d5fc7b7
hanweizhou <hanwei.zhou@my.jcu.edu.au>
2024-02-20 20:24:11 +0800

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,22 +1,9 @@
-MINIMUM_LENGTH = 6
+MINIMUM_LENTH = 6
 
-
-def main():
-    password = get_password(MINIMUM_LENGTH)
-    print_asterisks(password)
-
-
-def get_password(MINIMUM_LENGTH):
-    while True:
-        password = input("Enter a password: ")
-        if len(password) < MINIMUM_LENGTH:
-            print(f"Password must be at least {MINIMUM_LENGTH} characters long. Try again.")
-        else:
-            return password
-
-
-def print_asterisks(word):
-    print('*' * len(word))
-
-
-main()
+while True:
+    password = input("Enter password: ")
+    if len(password) < MINIMUM_LENTH:
+        print(f"fPassword must be at least {MINIMUM_LENTH} characters long. Try again.")
+    else:
+        print("*" * len(password))
+        break
