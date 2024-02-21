fa9430efda773eca509e63d53d837b96771cc667
hanweizhou <hanwei.zhou@my.jcu.edu.au>
2024-02-21 20:46:30 +0800

    add print result function

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,25 +1,20 @@
-
-"""
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    if score > 50:
-        print("Passable")
-    if score > 90:
-    print("Excellent")
-if score < 50:
-    print("Bad")
-"""
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+def main():
     score = float(input("Enter score: "))
-if score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-else:
-    print("Bad")
+    result = convert_score_to_result(score)
+    print(f"Your result is {result}")
+
+
+def convert_score_to_result(score):
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    if score > 90:
+        result = "Excellent"
+    elif score > 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
