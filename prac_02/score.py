3c7ffac2aba131158c0b22420af1a0efe717a2c1
hanweizhou <hanwei.zhou@my.jcu.edu.au>
2024-02-21 20:55:05 +0800

    made changes accroding to solutions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,20 +1,44 @@
+#
+# def main():
+#     score = float(input("Enter score: "))
+#     result = convert_score_to_result(score)
+#     print(f"Your result is {result}")
+#
+#
+#
+# def convert_score_to_result(score):
+#     determine the result
+#     while score < 0 or score > 100:
+#         print("Invalid score")
+#         score = float(input("Enter score: "))
+#     if score > 90:
+#         result = "Excellent"
+#     elif score > 50:
+#         result = "Passable"
+#     else:
+#         result = "Bad"
+#     return result
+#
+#
+# main()
+
+
 def main():
+    """Get a  score and display its result."""
     score = float(input("Enter score: "))
-    result = convert_score_to_result(score)
-    print(f"Your result is {result}")
+    print("Your result is", determine_status(score))
 
 
-def convert_score_to_result(score):
-    while score < 0 or score > 100:
-        print("Invalid score")
-        score = float(input("Enter score: "))
-    if score > 90:
-        result = "Excellent"
-    elif score > 50:
-        result = "Passable"
+def determine_status(score):
+    """Determine the result of a given score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
     else:
-        result = "Bad"
-    return result
+        return "Bad"
 
 
 main()
