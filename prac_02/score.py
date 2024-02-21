fa2a3294090ed3fd568b2faf0e5759296e5471a8
hanweizhou <hanwei.zhou@my.jcu.edu.au>
2024-02-21 20:16:39 +0800

    commit prac1 broken_score.py

---
+++ prac_02/score.py
@@ -0,0 +1,25 @@
+
+"""
+score = float(input("Enter score: "))
+if score < 0:
+    print("Invalid score")
+else:
+    if score > 100:
+        print("Invalid score")
+    if score > 50:
+        print("Passable")
+    if score > 90:
+    print("Excellent")
+if score < 50:
+    print("Bad")
+"""
+score = float(input("Enter score: "))
+while score < 0 or score > 100:
+    print("Invalid score")
+    score = float(input("Enter score: "))
+if score > 90:
+    print("Excellent")
+elif score > 50:
+    print("Passable")
+else:
+    print("Bad")
