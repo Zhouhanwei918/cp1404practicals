23064c99cf3b810304e642c697d2b0912cd652df
hanweizhou <hanwei.zhou@my.jcu.edu.au>
2024-03-03 22:39:47 +0800

    DONE

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+# question1 :
+#   I saw 1 number printed out
+#   The smallest number I saw is 5, the largest was 20
+
+# question2 :
+#   The smallest number I saw is 3, the largest was 9
+#   nop, because it only generates odd numbers
+
+# question3 :
+#   random floating-point number between 2.5 and 5.5
+#   the smallest number is 2.5, the largest is 5.5
+
+import random
+
+random_number = random.randint(1, 100)
+print(random_number)
