59d09a4ccd8bc5c295a94ac8d124a9d637470e81
hanweizhou <hanwei.zhou@my.jcu.edu.au>
2024-03-13 21:42:09 +0800

    Complete prac 5 hex_colours

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,19 @@
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba",
+                 "Acid Green": "#b0bf1a",
+                 "AliceBlue": "#f0f8ff",
+                 "Alizarin crimson": "#e32636",
+                 "Amaranth": "#e52b50",
+                 "Amber": "#ffbf00",
+                 "Amethyst": "#9966cc",
+                 "AntiqueWhite": "	#faebd7",
+                 "Apricot": "#fbceb1",
+                 "Aqua": "#00ffff"}
+
+print(COLOUR_TO_CODE)
+
+while True:
+    choose_color = input("Choose color: ").capitalize()
+    if choose_color in COLOUR_TO_CODE:
+        print("The color code for", choose_color, "is", COLOUR_TO_CODE[choose_color])
+    else:
+        print("Invalid color name. Please try again.")
