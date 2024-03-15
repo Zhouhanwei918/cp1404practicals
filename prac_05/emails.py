39997bb2a4ee3d74548f59bda3ea0686ed49c9eb
hanweizhou <hanwei.zhou@my.jcu.edu.au>
2024-03-15 16:01:14 +0800

    Complete prac 5 emails.py

---
+++ prac_05/emails.py
@@ -0,0 +1,31 @@
+"""
+Word Occurrences
+Estimate: 15 minutes
+Actual:    23minutes
+"""
+
+# email_to_name = {}
+#
+#
+# email = input("Email: ")
+# emails = email.split(".")
+# print(f"Is your name {emails[0]}? (Y/n)")
+
+
+email_to_name = {}
+
+email = input("Email: ")
+while email != "":
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+
+    confirmation = input(f"Is your name {name}? (Y/n) ")
+    if confirmation.upper() != "Y" and confirmation != "":
+        name = input("Name: ")
+
+    email_to_name[email] = name
+    email = input("Email: ")
+
+for email, name in email_to_name.items():
+    print(f"{name} ({email})")
